28e58aa3f8243a7db0ae95000900fcd81f52774f
Evez Memmedov <evezm254@gmail.com>
2025-08-21 18:41:43 +0000

    Added

--- execute.c
+++ execute.c
@@ -3,30 +3,31 @@
 /**
  * execute - proqramı işə salır
  * @args: argumentlər array
+ *
  * Return: 1 (davam etsin)
  */
 int execute(char **args)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    if (args[0] == NULL)
-        return (1);
+	if (args[0] == NULL)
+		return (1);
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-            perror("hsh");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid < 0)
-    {
-        perror("hsh");
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
-    return (1);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+			perror("hsh");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("hsh");
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+	return (1);
 }
